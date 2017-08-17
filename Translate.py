084e95a89d1156df7eba348a37ad809918bd899d
RJ Rosario <rrosa006@ucr.edu>
2017-08-16 18:43:10 -0700

    moved everythign inside of a function and deleted some unnecessary lines

--- Translate.py
+++ Translate.py
@@ -3,30 +3,27 @@ import bs4 as bs
 import urllib.request
 
 
-soup = urllib.request.urlopen('https://tabs.ultimate-guitar.com/e/emmure/10_signs_you_should_leave_tab.htm').read()
-soup = bs.BeautifulSoup(soup, 'lxml')
-contents = soup.find("pre", {"class": "js-tab-content js-copy-content js-tab-controls-item"})
-tabs = str(contents)
-digits = re.findall(r'(?<=-)(\d+)', tabs)
+def binary_tabs(link):
+    soup = urllib.request.urlopen(link).read()
+    soup = bs.BeautifulSoup(soup, 'lxml')
+    contents = soup.find("pre", {"class": "js-tab-content js-copy-content js-tab-controls-item"})
+    tabs = str(contents)
+    digits = re.findall(r'(?<=-)(\d+)', tabs)
 
+    test = [list(number) if len(number) >= 2 and number[0] == '0' and number[1] == '0'
+            else number for number in digits]
 
-test = [list(number) if len(number) >= 2 and number[0] == '0' and number[1] == '0'
-        else number for number in digits]
+    flattened = []
+    for element in test:
+        if type(element) == list:
+            for inside in element:
+                flattened.append(int(inside))
+        if type(element) != list:
+            flattened.append(int(element))
 
-flattened = []
-for element in test:
-    if type(element) == list:
-        for inside in element:
-            flattened.append(inside)
-    if type(element) != list:
-        flattened.append(element)
+    total = ''.join([format(x, 'b') if x > 1 else str(x) for x in flattened])
+    length = 8
 
+    return [total[i:i + length] for i in range(0, len(total), length)]
 
-convert_int = [int(x) for x in flattened]
-converted = [format(x, 'b') if x > 1 else str(x) for x in convert_int]
-total = ''.join(converted)
-length = 8
-
-binaryTabs = [total[i:i+length] for i in range(0, len(total), length)]
-print(binaryTabs)
-
+binary_tabs('https://tabs.ultimate-guitar.com/e/emmure/10_signs_you_should_leave_tab.htm')
